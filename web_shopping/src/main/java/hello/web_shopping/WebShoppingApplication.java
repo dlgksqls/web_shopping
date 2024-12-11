cbdfca4b40937fd372e09339cda456cb2121516e
dlgksqls <horainlee87@gmail.com>
2024-12-11 12:51:43 +0900

    초기 프로젝트 설정

---
+++ web_shopping/src/main/java/hello/web_shopping/WebShoppingApplication.java
@@ -0,0 +1,13 @@
+package hello.web_shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebShoppingApplication.class, args);
+	}
+
+}
---
+++ web_shopping/src/test/java/hello/web_shopping/WebShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.web_shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
